bf9c5319660477fb15da0fb997f14ecbd2f2d376
Xwen66 <xwenx66@gmail.com>
2025-08-24 21:19:58 -0700

    promptSystem adjustment

---
+++ Assets/Scripts/QuestSystem/Quest.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public abstract class Quest : MonoBehaviour
+{
+    public string QuestName;
+    public string QuestDescription;
+    private bool _isActive;
+    private bool _isCompleted;
+    private string _questHintText;
+    public abstract void GetHintText();
+}
